467425e57b6b3cda2978b5dd06d4b5c649c61d88
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-17 15:23:08 +0600

    calculator

---
+++ calculator.js
@@ -0,0 +1,37 @@
+let screen = document.getElementById("screen");
+screen.classList.add('p-4', 'font-medium');
+const buttons = document.querySelectorAll('.button-value');
+
+function updateScreenValue(buttonText) {
+    screen.value += buttonText;
+}
+
+for (const button of buttons) {
+    button.classList.add(
+      "p-4",
+      "text-center",
+      "font-medium",
+      "bg-green-400",
+      "rounded-full",
+      "cursor-pointer");
+    button.addEventListener('click', function (event) {
+        let buttonText = event.target.innerText;
+        if (buttonText == 'X') {
+            buttonText = '*';
+            updateScreenValue(buttonText);
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screen.value);
+        }
+        else if (buttonText == 'C') {
+            screen.value = screen.value.slice(0, -1);
+        }
+        else if (buttonText == 'Clear' || buttonText == 'emp') {
+            screen.value = '';
+        }
+        else {
+            updateScreenValue(buttonText);
+        }
+        }
+    )
+}
